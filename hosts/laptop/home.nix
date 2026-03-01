b1363b60fba5bdfd7046ee94596d7912438a69ef
Michael <mcmillanbmac@gmail.com>
2026-03-01 21:13:51 +1100

    Cleanup: Removed legacy NixOS files, keeping Fedora scripts

--- hosts/laptop/home.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Fedora handles the drivers; we just add the tools
-  home.packages = with pkgs; [
-    conda  # Works natively on Fedora
-    powertop # Laptop battery monitoring
-  ];
-}
